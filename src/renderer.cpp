e746ba4e8a941c7cb830df88dac088f457adb202
xiuxiu62 <jacremer@live.com>
2024-11-29 15:56:57 -0800

    progresses synapse renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -53,6 +53,34 @@ void main() {
 }
 )";
 
+const char *bloom_vertex_shader_source = R"(
+#version 430
+
+layout(location = 0) in vec2 position;
+layout(location = 0) in vec2 texcoord;
+
+out vec2 v_texcoord;
+
+void main() {
+    v_texcoord = texcoord;
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+)";
+
+const char *blur_vertex_shader_source = R"(
+#version 430
+
+layout(location = 0) in vec2 position;
+layout(location = 0) in vec2 texcoord;
+
+out vec2 v_texcoord;
+
+void main() {
+    v_texcoord = texcoord;
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+)";
+
 // In renderer.cpp, add line shaders:
 const char *synapse_vertex_shader_source = R"(
 #version 430
